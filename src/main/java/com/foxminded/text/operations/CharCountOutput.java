f07a1a649b8ed669c5b37196804be7e7fc1ade89
evergood <kanzann@gmail.com>
2019-10-31 23:16:23 +0300

    v4
    v4.1
    
    v4.1
    
    v4.2
    
    v4.3
    
    v4.4
    
    v4.5
    
    v4.6
    
    v4.7
    
    4.8
    
    v4.9
    
    v4.10
    
    final
    
    x
    
    x

---
+++ src/main/java/com/foxminded/text/operations/CharCountOutput.java
@@ -0,0 +1,8 @@
+package com.foxminded.text.operations;
+
+public class CharCountOutput {
+    public static void main(String[] args) {
+        CharactersCounter counter = new CharactersCounter();
+        System.out.println(counter.displayCharsCount("ffffrrrr ttthhhhhhhh."));
+    }
+}
